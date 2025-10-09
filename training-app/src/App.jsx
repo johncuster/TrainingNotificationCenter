7d0238d74c147ea4796515498b8f1a0d64682a28
Mark John Custer <mark_john_custer@dlsu.edu.ph>
2025-10-09 15:34:31 +0800

    new application
    developed a new backend using .jsx as file type.

---
+++ training-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, React JSX!</h1>
+      <p>This is your first React app running in Visual Studio Code 🎉</p>
+    </div>
+  );
+}
+
+export default App;
