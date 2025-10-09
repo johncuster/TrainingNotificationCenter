4110baa74352268d92e9e55a1b1345ea813832ad
Mark John Custer <mark_john_custer@dlsu.edu.ph>
2025-10-09 18:24:07 +0800

    App.jsx
    Changed the layout of the application. Will do the navbar layout first

--- training-app/src/App.jsx
+++ training-app/src/App.jsx
@@ -1,12 +1,73 @@
 import React from "react";
+//import rectangle16 from "./rectangle-16.svg";
+import "./style.css";
 
-function App() {
+export const App = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello, React JSX!</h1>
-      <p>This is your first React app running in Visual Studio Code 🎉</p>
+    <div className="manage-team">
+      <div className="group">
+        <div className="rectangle" />
+
+        <div className="text-wrapper">ADMIN DASHBOARD</div>
+      </div>
+
+      <header className="header">
+        <div className="team">
+          <div className="div" />
+
+          <div className="text-wrapper-2">MANAGE TEAM</div>
+        </div>
+
+        <div className="training">
+          <div className="rectangle-2" />
+
+          <div className="text-wrapper-3">MANAGE TRAINING</div>
+        </div>
+      </header>
+
+      <div className="footer" />
+
+      <div className="group-2" />
+
+      <div className="group-3">
+        <div className="rectangle-3" />
+
+        <div className="text-wrapper-4">+</div>
+      </div>
+
+      <div className="text-wrapper-5">MANAGE TEAM</div>
+
+      <div className="rectangle-4" />
+
+      <div className="rectangle-5" />
+
+    
+
+      <div className="rectangle-6" />
+
+      <div className="rectangle-7" />
+
+      <div className="rectangle-8" />
+
+      <div className="rectangle-9" />
+
+      <div className="group-4">
+        <div className="rectangle-10" />
+
+        <div className="rectangle-11" />
+
+        <div className="text-wrapper-6">TEAM NAME</div>
+
+        <div className="pending-completed">
+          Pending:
+          <br />
+          Completed:
+          <br />
+          Members:
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
